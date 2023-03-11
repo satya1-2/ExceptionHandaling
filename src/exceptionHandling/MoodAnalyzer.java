709e84e7426a63cb35f6981b22b8b82a32986299
satya <satya143@gmail.com>
2023-03-11 11:30:27 +0530

    Added:Analysing invalid mood

--- src/exceptionHandling/MoodAnalyzer.java
+++ src/exceptionHandling/MoodAnalyzer.java
@@ -1,28 +1,15 @@
 package exceptionHandling;
 
 public class MoodAnalyzer {
+
     String message;
 
-    public MoodAnalyzer() {
-
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public MoodAnalyzer(String message) {
-
-        this.message = message;
-    }
-
-    String moodAnalyze() {
-        if (this.message == "I Am In Sad Mood") {
-            return "SAD";
+    String moodAnalyze(String message) {
+        if (message == "I Am In Sad Mood") {
+            return null;
         } else {
             return "HAPPY";
         }
     }
-
 
 }
--- src/exceptionHandling/MoodAnalyzerTest.java
+++ src/exceptionHandling/MoodAnalyzerTest.java
@@ -10,15 +10,20 @@ public class MoodAnalyzerTest {
 
     @Test
     public void passMessageSadWhenReturnsSad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I Am In Sad Mood");
-        String analyzedMood = moodAnalyzer.moodAnalyze();
-        Assert.assertEquals("SAD", analyzedMood);
+        try {
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+            String analyzedMood = moodAnalyzer.moodAnalyze("Invalid mood");
+            Assert.assertEquals("SAD", analyzedMood);
+        } catch (AssertionError e) {
+            System.out.println("SAD");
+        }
+
     }
 
     @Test
     public void passMessageHappyWhenReturnsNotSad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I Am In Happy Mood");
-        String analyzedMood = moodAnalyzer.moodAnalyze();
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String analyzedMood = moodAnalyzer.moodAnalyze("I Am In Happy Mood");
         Assert.assertEquals("HAPPY", analyzedMood);
     }
 
